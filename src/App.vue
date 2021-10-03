22a852946a66a3b17f496463932606712046cce5
abdullahmasood553 <abdullahmasood553@gmail.com>
2021-10-03 13:22:22 -0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">
+        <div class="task" :key="product.id" v-for="product in products">
+           <h3>{{ product.title }}</h3>
+           <p>{{ product.description }}</p>
+        </div>
+          <div v-if="products.length" v-observe-visibility="handleScrolledToBottom"></div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+  data() {
+    return {
+     products: [],
+     page:1,
+     lastPage:1
+    }
+  },
+  methods: {
+    async fetch() {
+       let products = await axios.get(`http://localhost:8000/api/products/backend?page=${this.page}`)
+       // this.products = products.data.data
+       this.products.push(...products.data.data)
+       this.lastPage = products.data.last_page
+    },
+      handleScrolledToBottom(isVisible) {
+          if(!isVisible) {
+              return
+          }
+          if(this.page >= this.lastPage) {
+            return
+          }
+          this.page++;
+          this.fetch()
+      }
+  },
+  mounted() {
+    this.fetch()
   }
 }
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
+body {
+  font-family: 'Poppins', sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+
 </style>
